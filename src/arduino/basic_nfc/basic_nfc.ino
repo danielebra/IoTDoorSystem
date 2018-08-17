185ef0b2f1236831b9b36e8229714381addd356c
Daniel Ebrahimian <daniel@shimon.me>
2018-08-17 23:17:19 +1000

    Cleanup code

--- src/arduino/basic_nfc/basic_nfc.ino
+++ src/arduino/basic_nfc/basic_nfc.ino
@@ -1,29 +1,3 @@
-/**************************************************************************/
-/*! 
-    @file     iso14443a_uid.pde
-    @author   Adafruit Industries
-	@license  BSD (see license.txt)
-
-    This example will attempt to connect to an ISO14443A
-    card or tag and retrieve some basic information about it
-    that can be used to determine what type of card it is.   
-   
-    Note that you need the baud rate to be 115200 because we need to print
-	out the data and read from the card at the same time!
-
-This is an example sketch for the Adafruit PN532 NFC/RFID breakout boards
-This library works with the Adafruit NFC breakout 
-  ----> https://www.adafruit.com/products/364
- 
-Check out the links above for our tutorials and wiring diagrams 
-These chips use SPI or I2C to communicate.
-
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-*/
-/**************************************************************************/
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
@@ -31,35 +5,19 @@ products from Adafruit!
 
 Stepper_28BYJ_48 stepper(7,6,5,4);
 
-// If using the breakout with SPI, define the pins for SPI communication.
 #define PN532_SCK  (2)
 #define PN532_MOSI (3)
 #define PN532_SS   (4)
 #define PN532_MISO (5)
 
-// If using the breakout or shield with I2C, define just the pins connected
-// to the IRQ and reset lines.  Use the values below (2, 3) for the shield!
 #define PN532_IRQ   (2)
-#define PN532_RESET (3)  // Not connected by default on the NFC Shield
+#define PN532_RESET (3)
 
-// Uncomment just _one_ line below depending on how your breakout or shield
-// is connected to the Arduino:
-
-// Use this line for a breakout with a SPI connection:
-//Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
-
-// Use this line for a breakout with a hardware SPI connection.  Note that
-// the PN532 SCK, MOSI, and MISO pins need to be connected to the Arduino's
-// hardware SPI SCK, MOSI, and MISO pins.  On an Arduino Uno these are
-// SCK = 13, MOSI = 11, MISO = 12.  The SS line can be any digital IO pin.
-//Adafruit_PN532 nfc(PN532_SS);
-
-// Or use this line for a breakout or shield with an I2C connection:
 Adafruit_PN532 nfc(PN532_IRQ, PN532_RESET);
 
 void setup(void) {
   Serial.begin(115200);
-  Serial.println("Hello!");
+  Serial.println("Setting up...");
 
   nfc.begin();
 
@@ -69,20 +27,14 @@ void setup(void) {
     while (1); // halt
   }
   
-  // Got ok data, print it out!
   Serial.print("Found chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
   Serial.print('.'); Serial.println((versiondata>>8) & 0xFF, DEC);
   
-  // Set the max number of retry attempts to read from a card
-  // This prevents us from waiting forever for a card, which is
-  // the default behaviour of the PN532.
   nfc.setPassiveActivationRetries(0xFF);
-  
-  // configure board to read RFID tags
   nfc.SAMConfig();
   
-  Serial.println("Waiting for an ISO14443A card");
+  Serial.println("Waiting for a card...");
 }
 
 void loop(void) {
@@ -90,13 +42,10 @@ void loop(void) {
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };	// Buffer to store the returned UID
   uint8_t uidLength;				// Length of the UID (4 or 7 bytes depending on ISO14443A card type)
   
-  // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-  // 'uid' will be populated with the UID, and uidLength will indicate
-  // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
   success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
   
   if (success) {
-    Serial.println("Found a card!");
+    Serial.println("Card detected!");
     Serial.print("UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("UID Value: ");
     int val = 0;
@@ -107,7 +56,7 @@ void loop(void) {
     }
     // Daniel = 729
     // Ulash = 580
-    Serial.println("Total = ");// + val);
+    Serial.print(" : ");
     Serial.print(val);
     Serial.println("");
 
@@ -120,12 +69,10 @@ void loop(void) {
     {
       stepper.step(-60);
     }
-	// Wait 1 second before continuing
 	delay(1000);
   }
   else
   {
-    // PN532 probably timed out waiting for a card
     Serial.println("Timed out waiting for a card");
   }
 }
