e965403af1d66239df526c42153648bb16a1cb97
dalley <senghuotlay86@gmail.com>
2018-08-28 23:02:32 +1000

    Basic database integration setup with mongodb

---
+++ src/SSWebApp/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb://admin:admin1234@ds018258.mlab.com:18258/savage-security'
+} 
--- src/SSWebApp/server.js
+++ src/SSWebApp/server.js
@@ -1,5 +1,19 @@
-var express = require('express')
-var app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+//Body Parser
+app.use(bodyParser.json());
+
+//Database Configuration, get MongoURI
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db, {useNewUrlParser: true})
+    .then(() => console.log('database is connected to mongodb'))
+    .catch(err => console.log(err));
 
 app.use(express.static(__dirname))
 var server = app.listen(3000, () => {
