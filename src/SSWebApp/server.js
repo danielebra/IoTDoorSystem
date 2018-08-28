dfd9bc55f84a76388ed79edbadd7808bf737013b
dalley <senghuotlay86@gmail.com>
2018-08-28 23:24:06 +1000

    Setup react and integrate with both server

--- src/SSWebApp/server.js
+++ src/SSWebApp/server.js
@@ -16,6 +16,7 @@ mongoose
     .catch(err => console.log(err));
 
 app.use(express.static(__dirname))
-var server = app.listen(3000, () => {
+
+var server = app.listen(5000, () => {
     console.log('server is listening on port', server.address().port)
 })
