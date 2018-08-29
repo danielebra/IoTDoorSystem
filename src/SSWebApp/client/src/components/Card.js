0bb92e06005186375ccfbcce85baf8c644c45bd3
dalley <senghuotlay86@gmail.com>
2018-08-29 15:08:03 +1000

    Added logo

--- src/SSWebApp/client/src/components/Card.js
+++ src/SSWebApp/client/src/components/Card.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
-
-
+import utsDoor from './images/utsDoor.jpg'
 
 class Card extends Component {
     render() {
         return (
-            <div class="card" style={{width: "18rem"}}>
-                <img class="card-img-top" src="..." alt="Card image cap" />
+            <div class="card" style={{width: "18rem", textAlign:"center"}}>
+                <img width="100" class="card-img-top" src={utsDoor} alt="Card image cap"/>
                 <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <h5 class="card-title">UTS</h5>
                     <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
             </div>)
