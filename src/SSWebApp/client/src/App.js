ff5d3c5e157b6630d8ce08a6d76db3e3fa60fdb2
dalley <senghuotlay86@gmail.com>
2018-09-03 21:47:02 +1000

    Changed File name

--- src/SSWebApp/client/src/App.js
+++ src/SSWebApp/client/src/App.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-import Main from './routes/Main'
+import router from './routes/router'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" component={Main} />
+        <Route path="/" component={router} />
       </Router>
     );
   }
