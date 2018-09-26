04b41edcdeac8a6f8909b1f0d51661061ac45856
Daniel Ebrahimian <daniel@shimon.me>
2018-09-27 00:19:30 +1000

    Add notes about potential bug

--- src/SSWebApp/routes/api/AccessRequest.js
+++ src/SSWebApp/routes/api/AccessRequest.js
@@ -16,12 +16,15 @@ router.get('/:roomNumber/today', (req,res,next) => {
     const roomNumber = req.params.roomNumber
     // Force everything to Sydney time because the server is in America...
     let today = new Date(Date().toLocaleString("en-US", {timeZone: "Australia/Sydney"}))
-    
+
     accessRequestModel.find({"roomNumber": roomNumber}).lean()
         .exec(function(err,entry) {
             let output = entry.filter(e => {
 
                 let comparison = new Date(Date(e.timestamp).toLocaleString("en-US", {timeZone: "Australia/Sydney"}))
+                // Something weird might be going on here... requires investigation
+                console.log(e.timestamp)
+                console.log("Todays date: " + today.toDateString() + " being checked against: " + comparison.toDateString())
                 return comparison.toDateString() == today.toDateString()
             })
             res.send(output)
