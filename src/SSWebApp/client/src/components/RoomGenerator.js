d9bfbcd9f582b1650f423af066730d27c059d71a
Daniel Ebrahimian <daniel@shimon.me>
2018-10-02 17:13:29 +1000

    Generate RoomGenerator interface based on data from backend

--- src/SSWebApp/client/src/components/RoomGenerator.js
+++ src/SSWebApp/client/src/components/RoomGenerator.js
@@ -6,31 +6,7 @@ const room2 = require("../resources/images/rooms/room2.jpg")
 const room3 = require("../resources/images/rooms/room3.jpg")
 const room4 = require("../resources/images/rooms/room4.jpg")
 const room5 = require("../resources/images/rooms/room5.jpg")
-
-
-const existingRooms = [
-    {
-        name: "Small Classroom",
-        image: room1
-    },
-    {
-        name: "Large Classroom",
-        image: room2
-    },
-    {
-        name: "Dorm",
-        image: room3
-    },
-    {
-        name: "Very Small Classroom",
-        image: room4
-    },
-    {
-        name: "Lecture Hall",
-        image: room5
-    }
-]
-
+const axios = require('axios');
 
 class RoomGenerator extends Component {
     constructor(props)
@@ -40,20 +16,21 @@ class RoomGenerator extends Component {
     }
     componentDidMount()
     {
-        // TODO:
-        // Populate room information from database
-        // this.props.match.params.location
-
-        this.setState({
-            rooms: existingRooms
-        })
+        axios.get('/api/rooms/allRoomsByLocation/' + this.props.match.params.location)
+        .then(resp => 
+            {
+                console.log(resp.data)
+                this.setState({
+                rooms: resp.data
+                })
+            })
     }
     render() {
         return (
             <div style={{display: "flex", justifyContent:"space-around", flexWrap: "wrap"}}>
                 {this.state.rooms.map((item, index) => {
                     
-                    return <Card key={index} room={item.name} name={item.name} image={item.image} />
+                    return <Card key={index} room={item.name} name={item.name} image={room1} />
                     })
                 }
             </div>
