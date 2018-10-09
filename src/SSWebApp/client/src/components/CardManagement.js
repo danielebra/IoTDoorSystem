4eb9abd79e899a42ee0de83f12c956fddbcf98b3
Daniel Ebrahimian <daniel@shimon.me>
2018-10-09 16:00:40 +1100

    Create CardManagement template

--- src/SSWebApp/client/src/components/CardManagement.js
+++ src/SSWebApp/client/src/components/CardManagement.js
@@ -1,36 +1,59 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
-import Card from './Card';
+import  BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import { Dirent } from 'fs';
 const axios = require('axios');
 
-class RoomGenerator extends Component {
+
+
+
+class CardManagement extends Component {
     constructor(props)
     {
+        console.log("correctpage")
         super(props)
-        this.state = {rooms: []}
+        this.state = {cards: []}
+        this.columns = [
+            {
+                dataField: 'cardNumber',
+                text: 'Card Number',
+                sort: true
+            }, 
+            {
+                dataField: 'isActive',
+                text: 'Enabled',
+                sort: true
+            }, 
+            {
+                dataField: 'userId',
+                text: 'User ID',
+                sort: true
+            }
+        ];
     }
     componentDidMount()
     {
-        axios.get('/api/rooms/allRoomsByLocation/' + this.props.match.params.location)
+        axios.get('/api/cards/')
         .then(resp => 
             {
                 console.log(resp.data)
                 this.setState({
-                rooms: resp.data
+                cards: resp.data
                 })
             })
     }
     render() {
         return (
-            <div style={{display: "flex", justifyContent:"space-around", flexWrap: "wrap"}}>
-                {this.state.rooms.map((item, index) => {
-                    
-                    return <Card key={index} roomID={item._id} room={item._id} name={item.name} image={room1} />
-                    })
-                }
+            
+            <div style={{marginRight: 50}}>
+            <center><div><h1>Card Management</h1></div></center>
+            <BootstrapTable keyField='_id' data={ this.state.cards } columns={ this.columns } />
+            <p>hello</p>
+
             </div>
             )
     }
 }
 
-export default RoomGenerator;
+export default CardManagement;
