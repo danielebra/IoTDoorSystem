04acd889d0ef2c57961adc1d5ab4d4d1ed45afe2
Daniel Ebrahimian <daniel@shimon.me>
2018-10-13 16:00:29 +1100

    Add RoomManagement

---
+++ src/SSWebApp/client/src/components/AccessManager.js
@@ -0,0 +1,58 @@
+
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import BootstrapTable from 'react-bootstrap-table-next';
+import Card from './Card';
+const room1 = require("../resources/images/rooms/room1.jpg")
+const room2 = require("../resources/images/rooms/room2.jpg")
+const room3 = require("../resources/images/rooms/room3.jpg")
+const room4 = require("../resources/images/rooms/room4.jpg")
+const room5 = require("../resources/images/rooms/room5.jpg")
+const axios = require('axios');
+
+class AccessManager extends Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = {allowedCards: [{"foo":"bar"}]}//{allowedCards:["faooo"]}} 
+        this.columns = [
+            {
+                dataField: 'Card',
+                text: "Card ID",
+                sort: true
+            }
+        ]
+        console.log(this.props)
+    }
+    componentDidMount()
+    {
+        axios.get('/api/accessManager/5bbb566ea8f3830ced11d02c')// + this.props.match.params.location)
+        .then(resp => 
+            {
+                console.log(resp.data.allowedCards)
+                var pair = []
+                for (const x of resp.data.allowedCards)
+                {
+                    pair.push({"Card": x })
+                }
+                this.setState({
+                allowedCards: pair
+                })
+                console.log(this.state.allowedCards);
+            })
+    }
+    render() {
+        return (
+            <div>
+                <center><div><h1>Room Management for {this.props.match.params.room}</h1></div></center>
+                <div style={{marginRight: 50}}>
+
+                <BootstrapTable keyField='allowedCards' data={ this.state.allowedCards} columns={ this.columns } />
+                </div>
+                <div style={{display: "flex", justifyContent:"space-around", flexWrap: "wrap"}}/>
+            </div>
+            )
+    }
+}
+
+export default AccessManager;
--- src/SSWebApp/client/src/components/SideNavigationBar.js
+++ src/SSWebApp/client/src/components/SideNavigationBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SideNav, { Nav, NavIcon, NavText } from 'react-sidenav';
 import SvgIcon from 'react-icons-kit';
-
+import {userPlus} from 'react-icons-kit/icomoon/userPlus'
 import {addressCardO} from 'react-icons-kit/fa/addressCardO'
 import {users} from 'react-icons-kit/icomoon/users'
 import { ic_aspect_ratio } from 'react-icons-kit/md/ic_aspect_ratio';
@@ -37,6 +37,14 @@ class SideNavigationBar extends Component {
                             <NavIcon>
                                 <SvgIcon size={20} icon={users} />
                             </NavIcon>
+                        </Nav>
+                    </Link>
+                    <Link to={'/RoomManagement'}>
+                        <Nav id="RoomManagement">
+                            <NavIcon>
+                                <SvgIcon size={20} icon={userPlus} />
+                            </NavIcon>
+
                         </Nav>
                     </Link>
                 </SideNav>
